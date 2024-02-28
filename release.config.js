ed7a9c6d91ea3d9d31387f0b6f5deebb9c3caa88
FlipZero <86567519+FlipZ3ro@users.noreply.github.com>
2024-02-28 12:02:24 +0700

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -1,13 +1,14 @@
 module.exports = {
     extends: 'scratch-semantic-release-config',
     branches: [
+        'main', // Tambahkan branch default 'main' di sini atau sesuaikan dengan branch default Anda
         {
-            name: 'develop'
-            // default channel
+            name: 'develop',
+            channel: 'next' // Tentukan saluran 'next' untuk branch 'develop'
         },
         {
             name: 'hotfix/*',
-            channel: 'hotfix',
+            channel: 'hotfix', // Tentukan saluran 'hotfix' untuk branch 'hotfix/*'
             prerelease: 'hotfix'
         }
     ]
