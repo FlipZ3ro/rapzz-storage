dd177fcea9fb7033ba5bd34a458634880a9178a1
FlipZero <86567519+FlipZ3ro@users.noreply.github.com>
2024-02-28 12:14:52 +0700

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ['main', 'develop', 'hotfix/*'],
+  branches: ['main'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
