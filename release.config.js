d5e24dff1722d0f5048eedf4727e4b9620fc1921
FlipZero <86567519+FlipZ3ro@users.noreply.github.com>
2024-02-28 12:16:38 +0700

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-  branches: ['main'],
+  branches: ['main', 'develop', { name: 'hotfix/*', channel: 'hotfix', prerelease: 'hotfix' }],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@semantic-release/npm',
-    '@semantic-release/github'
-  ]
+    '@semantic-release/github',
+    ['@semantic-release/git', {
+      assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+    }],
+  ],
 };
